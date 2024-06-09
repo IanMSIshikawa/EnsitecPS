5ca6c92b7f34a966bd31f91931c4b4811af92594
IanMSIshikawa <ianishikawa1@gmail.com>
2024-06-08 22:51:55 -0300

    front end da pagina inicial iniciado

---
+++ src/pages/initPage.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app" class="container">
+        <h1>{{ titulo }}</h1>
+        <p> Pesquise, edite, insira ou delete produtos do estoque <br>
+            Selecione uma das opções abaixo para prosseguir
+        </p>
+        <div class="button_container">
+          <button> {{ botoes[0] }} </button>
+          <button> {{ botoes[1] }} </button>
+          <button> {{ botoes[2] }} </button>
+          <button> {{ botoes[3] }} </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Estoque fácil',
+      botoes: ['Pesquisar', 'Editar', 'Inserir', 'Deletar']
+    };
+  }
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+}
+
+.button_container button{
+  text-align: center;
+  margin-top: 40px;
+  margin-left: 20px;
+  margin-right: 20px;
+  scale: 1.5;
+}
+
+h1 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+p {
+  margin-top: 20px;
+  font-size: 18px;
+}
+</style>
